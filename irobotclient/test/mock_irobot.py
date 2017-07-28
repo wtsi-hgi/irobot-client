bb76f65922fcf9cec60b52c34080021f9f5ad23d
EWeatherill <ew7@sanger.ac.uk>
2017-07-28 13:12:19 +0100

    Python unittest skeleton

---
+++ irobotclient/test/mock_irobot.py
@@ -0,0 +1,37 @@
+import unittest
+
+# class _MockRequest(unittest.TestCase):
+#     """
+#
+#     """
+#     def setUp(self):
+#         # set up request but how?
+#
+# class TestResponses(unittest.TestCase): # Too general I think
+#     """
+#
+#     """
+#     def setUp(self):
+#         # set up request?
+#         # set up response?
+#
+#     # Test wait response
+#         # request a cram or bam file
+#         # return 202 response for cram or bam
+#         # recieve delayed request
+#         # return 200 response
+#
+#     # Test file with no extension specified
+#         # return 200 response
+#
+#     # Test failure codes
+#
+#
+# # Test inputs?
+
+# Start simple
+
+
+
+if __name__ == '__main__':
+    unittest.main()
