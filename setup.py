e1049b8710d8db3b4578b054eb897964232e0ada
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-02-15 14:44:44 +0000

    Releases version 1.0.0.

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 from setuptools import setup, find_packages
 
-setup(name='irobotclient',
-      version='0.1',
-      description='Client for the iRobot HTTP API',
-      url='https://github.com/wtsi-hgi/irobot-client',
-      author='Elizabeth Weatherill',
-      author_email='elizabeth.weatherill@sanger.ac.uk',
-      license='GNU General Public License',
+try:
+    from pypandoc import convert
+    def read_markdown(file: str) -> str:
+        return convert(file, "rst")
+except ImportError:
+    def read_markdown(file: str) -> str:
+        return open(file, "r").read()
+
+
+setup(name="irobotclient",
+      version="1.0.0",
+      description="Client for the iRobot HTTP API",
+      url="https://github.com/wtsi-hgi/irobot-client",
+      author="Elizabeth Weatherill",
+      author_email="elizabeth.weatherill@sanger.ac.uk",
+      license="GNU General Public License",
       packages=find_packages(exclude=["test"]),
       install_requires=open("requirements.txt", "r").readlines(),
       entry_points={
-          'console_scripts': [
-              'irobotclient=irobotclient.entrypoint:main']},
-      zip_safe=False)
+          "console_scripts": [
+              "irobotclient=irobotclient.entrypoint:main"
+          ]
+      },
+      zip_safe=True)
